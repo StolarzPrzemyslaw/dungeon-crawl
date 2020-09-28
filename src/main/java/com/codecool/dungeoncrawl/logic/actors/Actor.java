ddd977270a48d3d67a9a7d40333e19a0b3e4b946
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-09-28 16:55:51 +0200

    Modified Actor class and change localisation of move method

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -4,23 +4,11 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Actor implements Drawable {
-    private Cell cell;
-    private int health = 10;
+    protected Cell cell;
 
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
-    }
-
-    public void move(int dx, int dy) {
-        Cell nextCell = cell.getNeighbor(dx, dy);
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
-    }
-
-    public int getHealth() {
-        return health;
     }
 
     public Cell getCell() {
