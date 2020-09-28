28bce02c1f5d20da0e14a66c3749100ac29dd5e4
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-09-28 16:57:16 +0200

    Add two abstract subclasses to divide objects into smaller types

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Item.java
@@ -0,0 +1,19 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public abstract class Item extends Actor {
+
+    public Item(Cell cell) {
+        super(cell);
+    }
+
+    public void getItemFromFloor() {
+
+    }
+
+    public void useItem() {
+
+    }
+
+}
