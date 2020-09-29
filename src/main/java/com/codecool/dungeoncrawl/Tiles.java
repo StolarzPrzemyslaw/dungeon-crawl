13411b5d6bc5e983b3db3c89646a6de19437c1ac
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-09-29 16:04:10 +0200

    Add closed and open door tiles to tileMap in Tiles class

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -31,6 +31,8 @@ public class Tiles {
         tileMap.put("player", new Tile(27, 0));
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("cow", new Tile(27, 7));
+        tileMap.put("closedDoor", new Tile(0, 9));
+        tileMap.put("openDoor", new Tile(2, 9));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
