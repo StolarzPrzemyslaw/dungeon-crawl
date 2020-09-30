184d8c074d18ff924be496f787366e662890fda0
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-09-30 22:17:08 +0200

    Add private field into Cow to hold information about steps done and steps to do

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/characters/Cow.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/characters/Cow.java
@@ -5,6 +5,9 @@ import com.codecool.dungeoncrawl.logic.actors.Actor;
 
 public class Cow extends Enemy {
 
+    private boolean isMovingLeft = true;
+    private int stepLeft = 6;
+
     public Cow(Cell cell) {
         super(cell);
         this.strength = 0;
@@ -14,6 +17,26 @@ public class Cow extends Enemy {
 
     public void runActionAfterDefeat(Player player) {
         player.healUp(20);
+    }
+
+    public void resetStepsLeft() {
+        this.stepLeft = 6;
+    }
+
+    public void decreaseStepsLeft() {
+        this.stepLeft -= 1;
+    }
+
+    public int getStepsLeft() {
+        return this.stepLeft;
+    }
+
+    public boolean isMovingLeft() {
+        return this.isMovingLeft;
+    }
+
+    public void swapDirectionOfMoving() {
+        this.isMovingLeft  = !this.isMovingLeft;
     }
 
     public String getTileName() {
