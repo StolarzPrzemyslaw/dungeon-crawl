2fb1b6e094b6ca381cf3483b2667c0c46328d1f3
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-01 09:40:52 +0200

    Add message with damage statistics

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/components/Combat.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/components/Combat.java
@@ -1,23 +1,31 @@
 package com.codecool.dungeoncrawl.logic.actors.components;
 
 import com.codecool.dungeoncrawl.logic.actors.characters.*;
+import com.codecool.dungeoncrawl.view.Game;
 
 public class Combat {
+
+    Game ui;
+
+    public Combat(Game game) {
+        this.ui = game;
+    }
 
     public void simulateCombat(Person player, Person enemy) {
         enemy.setCurrentHealth(enemy.getCurrentHealth() - player.getStrength());
         player.setCurrentHealth(player.getCurrentHealth() - enemy.getStrength());
-        if (isEnemyDestroyed(player, enemy)) {
+        ui.displayLog("You get " + enemy.getStrength() + " damage and Enemy get " + player.getStrength() + " damage!");
+        if (isEnemyDestroyed(enemy)) {
             destroyEnemy(player, enemy);
         }
     }
 
-    private boolean isEnemyDestroyed(Person player, Person enemy) {
-        return enemy.getCurrentHealth() <= player.getStrength();
+    private boolean isEnemyDestroyed(Person enemy) {
+        return enemy.getCurrentHealth() <= 0;
     }
 
     public void destroyEnemy(Person player, Person enemy) {
-        ((Enemy) enemy).runActionAfterDefeat((Player) player);
+        ((Enemy) enemy).runActionAfterDefeat(ui, (Player) player);
         enemy.getCell().setActor(null);
     }
 }
