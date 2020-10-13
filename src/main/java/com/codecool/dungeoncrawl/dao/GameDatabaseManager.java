041de87b39b0dea90b4b2a00ee23bd7308a70cb6
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-13 12:27:30 +0200

    Add test usage method in GameDatabaseManager

--- src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
@@ -1,6 +1,8 @@
 package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.logic.actors.characters.Player;
+import com.codecool.dungeoncrawl.logic.actors.components.Inventory;
+import com.codecool.dungeoncrawl.model.InventoryModel;
 import com.codecool.dungeoncrawl.model.PlayerModel;
 import org.postgresql.ds.PGSimpleDataSource;
 
@@ -9,15 +11,29 @@ import java.sql.SQLException;
 
 public class GameDatabaseManager {
     private PlayerDao playerDao;
+    private ItemDao itemDao;
+    private InventoryDao inventoryDao;
 
     public void setup() throws SQLException {
         DataSource dataSource = connect();
         playerDao = new PlayerDaoJdbc(dataSource);
+        itemDao = new ItemDaoJdbc(dataSource);
+        inventoryDao = new InventoryDaoJdbc(dataSource, itemDao);
+    }
+
+    public void saveGameState() {
+
     }
 
     public void savePlayer(Player player) {
         PlayerModel model = new PlayerModel(player);
         playerDao.add(model);
+    }
+
+    public int saveInventoryReturnInventoryId(Inventory inventory) {
+        InventoryModel model = new InventoryModel(inventory);
+        inventoryDao.add(model);
+        return model.getId();
     }
 
     private DataSource connect() throws SQLException {
