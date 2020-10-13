0b076c76837e9417f21509fd5b46a629f0443036
marcinchaluda <chaluda@interia>
2020-10-13 15:01:36 +0200

    add getAll method in player DAO Jdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -1,10 +1,10 @@
 package com.codecool.dungeoncrawl.dao;
 
-import com.codecool.dungeoncrawl.logic.actors.items.Weapon;
 import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerDaoJdbc implements PlayerDao {
@@ -20,21 +20,27 @@ public class PlayerDaoJdbc implements PlayerDao {
             String sql = "INSERT INTO player (health, current_health, strength, name, posX, posY, " +
                     "inventory_id, weapon_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setInt(1, player.getHp());
-            statement.setInt(2, player.getCurrentHp());
-            statement.setInt(3, player.getStrength());
-            statement.setString(4, player.getPlayerName());
-            statement.setInt(5, player.getX());
-            statement.setInt(6, player.getY());
-            statement.setInt(7, player.getInventoryId());
-            statement.setInt(8, player.getWeaponId());
-            statement.executeUpdate();
+
+            insertPlayerDetails(player, statement);
+
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
             player.setId(resultSet.getInt(1));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void insertPlayerDetails(PlayerModel player, PreparedStatement statement) throws SQLException {
+        statement.setInt(1, player.getHp());
+        statement.setInt(2, player.getCurrentHp());
+        statement.setInt(3, player.getStrength());
+        statement.setString(4, player.getPlayerName());
+        statement.setInt(5, player.getX());
+        statement.setInt(6, player.getY());
+        statement.setInt(7, player.getInventoryId());
+        statement.setInt(8, player.getWeaponId());
+        statement.executeUpdate();
     }
 
     @Override
@@ -52,16 +58,7 @@ public class PlayerDaoJdbc implements PlayerDao {
             if (!resultSet.next()) {
                 return null;
             }
-            int health = resultSet.getInt(1);
-            int currentHealth = resultSet.getInt(2);
-            int strength = resultSet.getInt(3);
-            String name = resultSet.getString(4);
-            int posX = resultSet.getInt(5);
-            int posY = resultSet.getInt(6);
-            int inventoryId = resultSet.getInt(7);
-            int weaponId = resultSet.getInt(8);
-
-            PlayerModel playerModel = new PlayerModel(health, currentHealth, strength, name, posX, posY, inventoryId, weaponId);
+            PlayerModel playerModel = createPlayerModel(resultSet);
             playerModel.setId(id);
             return playerModel;
         } catch (SQLException e) {
@@ -69,25 +66,34 @@ public class PlayerDaoJdbc implements PlayerDao {
         }
     }
 
-    private Weapon getWeapon(int weaponId) {
-        try (Connection conn = dataSource.getConnection()) {
-            String sql = "SELECT name FROM item WHERE id = ?";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setInt(1, weaponId);
-            ResultSet resultSet = statement.executeQuery();
-            if (!resultSet.next()) {
-                return null;
-            }
-            String weaponName = resultSet.getString(1);
-            Weapon weapon = null;
-            return weapon;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    private PlayerModel createPlayerModel(ResultSet resultSet) throws SQLException {
+        int health = resultSet.getInt(2);
+        int currentHealth = resultSet.getInt(3);
+        int strength = resultSet.getInt(4);
+        String name = resultSet.getString(5);
+        int posX = resultSet.getInt(6);
+        int posY = resultSet.getInt(7);
+        int inventoryId = resultSet.getInt(8);
+        int weaponId = resultSet.getInt(9);
+
+        return new PlayerModel(health, currentHealth, strength, name, posX, posY, inventoryId, weaponId);
     }
 
     @Override
     public List<PlayerModel> getAll() {
-        return null;
+        List<PlayerModel> players = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM player";
+            ResultSet resultSet = conn.createStatement().executeQuery(sql);
+
+            while (resultSet.next()) {
+                PlayerModel playerModel = createPlayerModel(resultSet);
+                players.add(playerModel);
+            }
+            return players;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
