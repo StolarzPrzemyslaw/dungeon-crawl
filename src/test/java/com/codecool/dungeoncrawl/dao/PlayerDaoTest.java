ace9a92bcf6a43b0c21444f824c30201ab1fdebd
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-14 14:37:38 +0200

    Modify gitignore file and Add TestClasses into project

---
+++ src/test/java/com/codecool/dungeoncrawl/dao/PlayerDaoTest.java
@@ -0,0 +1,7 @@
+package com.codecool.dungeoncrawl.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerDaoTest {
+
+}
---
+++ src/test/java/com/codecool/dungeoncrawl/logic/actors/items/ItemTest.java
@@ -0,0 +1,6 @@
+package com.codecool.dungeoncrawl.logic.actors.items;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+}
