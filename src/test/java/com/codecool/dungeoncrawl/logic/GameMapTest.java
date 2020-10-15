acd555e0b18391bd3edd4e0ab564286c67fd1271
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-15 19:33:10 +0200

    Add small tests for GameMap class

--- src/test/java/com/codecool/dungeoncrawl/logic/GameMapTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/GameMapTest.java
@@ -1,7 +1,32 @@
 package com.codecool.dungeoncrawl.logic;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameMapTest {
+
+    private static GameMap gameMap;
+
+    @BeforeAll
+    public static void prepareGameMap() {
+        gameMap = new GameMap(30, 30, CellType.FLOOR, Map.LEVEL1);
+    }
+
+    @Test
+    public void getLevelId_runGetLevelIdOnMap_ReturnCorrectValue() {
+        assertEquals(1, gameMap.getLevelId());
+    }
+
+    @Test
+    public void getWidth_runGetWidthOnMap_ReturnCorrectValue() {
+        assertEquals(30, gameMap.getWidth());
+    }
+
+    @Test
+    public void getWidth_runGetHeightOnMap_ReturnCorrectValue() {
+        assertEquals(30, gameMap.getHeight());
+    }
 
 }
